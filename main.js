1ed24ac321f73d5097c84a4f4d3aa850115b313e
Ettie Fishman <123407548+ettiehi@users.noreply.github.com>
2023-09-18 08:31:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 
+class Container {
+    constructor(item) {
+        this.item = item;
+    }
+    serializeItem() {
+        return this.item.serialize();
+    }
+}
+
+class Employee {
+    constructor(name, education) {
+        this.name = name;
+        this.education = education;
+    }
+    serialize() {
+        return JSON.stringify({ name: this.name, education: this.education });
+    }
+}
+
+function serializeEmployee() {
+    const name = document.getElementById('name').value;
+    const education = document.getElementById('education').value;
+    const employee = new Employee(name, education);
+    const container = new Container(employee);
+    document.getElementById('output').innerText = container.serializeItem();
+}
